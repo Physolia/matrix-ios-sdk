fe542f9235c9f071dbdc7b0e540cd3dc3962b30d
Doug <douglase@element.io>
2022-04-01 16:26:33 +0100

    Add MXUsernameAvailability and an msisdn MXLoginFlowType.

--- MatrixSDK/JSONModels/MXJSONModels.m
+++ MatrixSDK/JSONModels/MXJSONModels.m
@@ -131,6 +131,21 @@ NSString *const kMXLoginIdentifierTypePhone = @"m.id.phone";
 
 @end
 
+@implementation MXUsernameAvailability
+
++ (id)modelFromJSON:(NSDictionary *)JSONDictionary
+{
+    MXUsernameAvailability *availability = [[MXUsernameAvailability alloc] init];
+    if (availability)
+    {
+        MXJSONModelSetBoolean(availability.available, JSONDictionary[@"available"]);
+    }
+    
+    return availability;
+}
+
+@end
+
 @implementation MXAuthenticationSession
 
 + (id)modelFromJSON:(NSDictionary *)JSONDictionary
